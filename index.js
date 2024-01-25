7cf652ae71edc7608646c6b15f2e0b96fe69bc86
Berezovsky Alexey <lazfnol@gmail.com>
2024-01-25 15:59:03 +0300

    add calculator's methods

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 // @ts-check
-
+import readlineSync from 'readline-sync';
 import half from './src/half.js';
 
 export default half;
+
+const calculator = {};
+
+calculator.read = () => {
+  calculator.a = readlineSync.question('type in first number: ');
+  calculator.b = readlineSync.question('type in second number: ');
+};
+
+calculator.sum = () => Number(calculator.a) + Number(calculator.b);
+calculator.mul = () => Number(calculator.a) * Number(calculator.b);
+
+calculator.read();
+console.log(calculator.sum());
+console.log(calculator.mul());
